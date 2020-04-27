ca2f47c2ce305a164132a0c37dbf249678a1802b
keita.aiba <keyfree.4322@gmail.com>
2020-04-27 13:48:56 +0000

    validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,20 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @messages = Message.all
   end
-  
+
   def show
   end
-  
+
   def new
     @message = Message.new
   end
-  
+
   def create
     @message = Message.new(message_params)
-    
+
     if @message.save
       flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
@@ -23,12 +23,11 @@ class MessagesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
-    
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -37,16 +36,16 @@ class MessagesController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @message.destroy
-    
+
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
-  
+
   def set_message
     @message = Message.find(params[:id])
   end
@@ -54,8 +53,4 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:content)
   end
-  
 end
-
-
-
